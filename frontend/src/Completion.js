5c6ba7017c44f58b05be188b35c8f7bef0d2a77b
benothmenons <benothmen.ons@esprit.tn>
2023-07-12 12:19:52 +0100

    page de verification

--- frontend/src/Completion.js
+++ frontend/src/Completion.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import "./completion.css";
+import tick  from "./assets/verifier.png"
+
+
 function Completion(props) {
-    return <h1>Thank you! 🎉</h1>;
-  }
-  
-  export default Completion;
+  // Custom logic or functionality can be added here
+  console.log(props);
+
+  return (
+    <div>
+      <img id="verified-logo" src={tick} alt="Verified Logo"  />
+    <h1>Payment succeeded</h1>
+      <p> thanks for your purchase! A payment from Stripe Sticker Shop will appear on your statment </p>
+      </div>
+    
+  );
+}
+
+export default Completion;
