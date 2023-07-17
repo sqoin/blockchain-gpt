853e0b999793ad9b4d9dafa5917c2a3c4dc74ba0
oueslatioussema <oussema.oueslati123@gmail.com>
2023-07-17 11:13:45 +0100

    free and paid accounts statistics per month

--- analytics/user-behaviour/index.ts
+++ analytics/user-behaviour/index.ts
@@ -63,7 +63,58 @@ app.get("/user-behaviour/PaidAccounts", (req:any, res:any) => {let result = [
   { name: "freeAccounts", count: 15 },
   { name: "PaidAccounts", count:30 }]
 
+  
+
+    
+
   res.send(result);
+
+
+
+  
+
+});
+
+
+
+app.get("/user-behaviour/PaidAccountsYEAR", (req:any, res:any) => {
+  var date = new Date();
+  let result = [
+  { name: "freeAccounts", count: 2, date:"202207"},
+  { name: "PaidAccounts", count:3 , date:"202207"},
+  { name: "freeAccounts", count: 3, date:"202208"},
+  { name: "PaidAccounts", count:7 , date:"202208"},
+  { name: "freeAccounts", count: 4, date:"202209"},
+  { name: "PaidAccounts", count:10 , date:"202209"},
+  { name: "freeAccounts", count: 6, date:"202210"},
+  { name: "PaidAccounts", count:11 , date:"202210"},
+  { name: "freeAccounts", count: 7, date:"202211"},
+  { name: "PaidAccounts", count:14 , date:"202211"},
+  { name: "freeAccounts", count: 9, date:"202212"},
+  { name: "PaidAccounts", count:17 , date:"202212"},
+  { name: "freeAccounts", count: 10, date:"202301"},
+  { name: "PaidAccounts", count:19 , date:"202301"},
+  { name: "freeAccounts", count: 11, date:"202302"},
+  { name: "PaidAccounts", count:21 , date:"202302"},
+  { name: "freeAccounts", count: 11, date:"202303"},
+  { name: "PaidAccounts", count:23 , date:"202303"},
+  { name: "freeAccounts", count: 12, date:"202304"},
+  { name: "PaidAccounts", count:25 , date:"202304"},
+  { name: "freeAccounts", count: 13, date:"202305"},
+  { name: "PaidAccounts", count:26 , date:"202305"},
+  { name: "freeAccounts", count: 14, date:"202306"},
+  { name: "PaidAccounts", count:28 , date:"202306"},
+  { name: "freeAccounts", count: 15, date:"202307"},
+  { name: "PaidAccounts", count:30 , date:"202307"}]
+  
+
+  
+
+    
+
+  res.send(result);
+
+
 
   
 
