fbc31be49cc711d4e76281d771321674a7b48c32
your_github_name <benothmen.ons@esprit.tn>
2023-07-27 13:50:09 +0100

    update version

---
+++ backend/chatdata/app.ts
@@ -0,0 +1,70 @@
+import { response } from 'express';
+import { chat, Ichat } from './model';
+
+const express = require('express');
+const fs = require('fs');
+const cors = require("cors");
+const app = express();
+const axios= require("axios")
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes.ts');
+
+
+
+
+app.use(express.json());
+
+
+/*async function sendHelloMessageToAll() {
+  try {
+    // Fetch all chat IDs from the database
+    const chatIds = await chat.find({}); // Only retrieve the 'chatId' field
+
+    console.log("start sending alerts..", chatIds);
+
+    // Send the "hello message" to each chat ID using the custom function
+    chatIds.forEach((chat) => {
+      const telegramApiUrl = `https://api.telegram.org/bot5856888046:AAH4B7M6Rcei9RFvlyYlbmNWMscMjmKUuRs/sendMessage?chat_id=${chat.chatId}&text=hello`;
+      axios.get(telegramApiUrl)
+        .then((response:any) => {
+          console.log('Message sent to Telegram:',response.data);
+        })
+        .catch((error:any) => {
+          console.error('Error sending message to Telegram:', error);
+        });
+    });
+  } catch (error) {
+    console.error('Error fetching chat IDs:', error);
+  }
+}*/
+// Endpoint to receive and store the chat ID
+app.post('/api/telegram/chat', (req:any, res:any) => {
+  const { chatId } = req.body;
+  //sendHelloMessageToAll();
+
+  console.log('Received chat ID:', chatId);
+
+  // Store the chat ID in a file
+ 
+  fs.appendFile('chatIds.json', chatId + '\n', (err:any) => {
+    if (err) {
+      console.error('Error storing chat ID:', err);
+      res.status(500).json({ message: 'Failed to store chat ID' });
+    } else {
+      console.log('Chat ID stored:', chatId);
+      res.status(200).json({ message: 'Chat ID stored successfully' });
+      //sendHelloMessageToAll();
+    //  console.log("message telegram recu ")
+
+    }
+  } );
+});
+
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+
+const port = 3006; // Specify the port number you want to listen on
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
+});
