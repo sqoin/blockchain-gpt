37ef971228c041efcd069011e5fec3ecb2a8cc1b
your_github_name <benothmen.ons@esprit.tn>
2023-07-28 11:35:43 +0100

    changement de bot

--- frontend/src/components/telegram-message/TelegramMessage.jsx
+++ frontend/src/components/telegram-message/TelegramMessage.jsx
@@ -6,14 +6,15 @@ const TelegramMessage = () => {
   const handleButtonClick = async () => {
     try {
       // Open the Telegram bot link in a new tab
-      window.open('http://t.me/testons_bot','_blank');
+      window.open('http://t.me/sqqoiin_bot','_blank');
       // Retrieve the chat ID from Telegram
       const chatId = await getChatIdFromTelegram();
       console.log(chatId);
 
       // Send the chat ID to your backend
-      await axios.post('http://localhost:3005/api/telegram/chat', { chatId });
+      await axios.post('http://localhost:3006/api/telegram/chat', { chatId });
       // Perform any other desired actions
+      
       console.log('Button clicked, chat ID collected, and sent to the backend.');
     } catch (error) {
       console.error('Error handling button click:', error);
@@ -26,7 +27,7 @@ const getChatIdFromTelegram = async () => {
     // Access the Telegram API or use any other method to obtain the chat ID
     // For example, you can use Telegram's Web Login Widget or similar approaches
     // Return the chat ID here
-    const response = await axios.get('https://api.telegram.org/bot6357082334:AAGmKJY-OlGXbEjdfpmRWSp935ogpHCQW5g/getUpdates');
+    const response = await axios.get('https://api.telegram.org/bot6572515145:AAH3lQky2jdYWs84nH0ZOf_-AnroOH3NGXs/getUpdates');
     const chatId = response.data.result[0].message.chat.id;
     
 return chatId;
