62fd200dbf92be288725acc55067be612c188a0b
yasmine <maazounyasmine2003@gmail.com>
2023-07-31 11:00:21 +0100

    added piechart

---
+++ frontend/src/components/Statistic/Charts.tsx
@@ -0,0 +1,13 @@
+import BarChart from "./Bar_Chart"
+import PieChart from "./Pie_Chart"
+
+const Charts = () => {
+    return (
+        <div>
+            <BarChart/>
+            <PieChart/>
+        </div>
+    )
+ }
+
+ export default Charts
