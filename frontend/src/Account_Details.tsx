a530f090e577974c75c9a4d03bc50955edba9dfb
Wael Wasli <wael.wasli@enicar.ucar.tn>
2023-08-09 13:28:58 +0200

    Account Details front/back : updated

--- frontend/src/Account_Details.tsx
+++ frontend/src/Account_Details.tsx
@@ -7,35 +7,29 @@ import { ACCOUNT_MANAGEMENT } from './utils/constants';
 import './Account_Details.css';
 import { FaTelegram } from "react-icons/fa"
 
-
-
-interface AccountDetailsProps {
-  user: {
-    email: string;
-    name: string;
-    githubAccount: string ;
-    googleAccount: string;
-    blockchainAccount: string;
-  };
-  userId: string; 
+interface IUser {
+  ID: string;
+  creation_date: Date;
+  expiration_date: Date;
+  name: string;
+  lastName: string;
+  email: string;
+  account_status: string;
+  telegram_user_name: string;
 }
 
-
-
-
 const AccountDetails: React.FC<any> = ({ userId }) => {
+  const [user, setUser] = useState<IUser | null>(null);
   const [isToggled, setIsToggled] = useState(false);
   const history = useHistory();
-  const [userInfo, setUserInfo] = useState({email: '', githubAccount: '',userName:'',userLastName:'' });
+  const [userInfo, setUserInfo] = useState({ email: '', githubAccount: '', userName: '', userLastName: '', telegram_user_name: '' });
   const [lastName, setLastName] = useState('');
-  const [User, setUser] = useState({ githubAccount: '', name: '', lastName: '' });
   const [Name, setName] = useState('');
   const session = useSessionContext();
-  if (!session.loading){
-    userId= session.userId;
+  if (!session.loading) {
+    userId = session.userId;
   }
 
-  // Function to handle changes to the last name
   const handleLastNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setLastName(event.target.value);
   };
@@ -43,54 +37,54 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
     setName(event.target.value);
   };
 
-
   const handleApplyChanges = () => {
-    UpdateUserInformation(userId, Name, lastName);
+    UpdateUserInformation(userId, Name, lastName, user?.telegram_user_name);
   };
 
 
-  async function UpdateUserInformation(userId: string, userName: string, userLastName: string) {
+  async function UpdateUserInformation(userId: string, userName: string, userLastName: string, telegramusername?: string) {
     try {
       const response = await axios.put(`${ACCOUNT_MANAGEMENT}/api/updateUser`, {
-        userId,
-        userName,
-        userLastName,
+        
+          userId,
+          userName:userName,
+          userLastName:userLastName,
+          telegram_user_name: telegramusername,
+        
       });
-      if (response.data && response.data.message) {
+  
+      if (response.status === 200) {
+        console.log('User updated successfully!');
+      } else {
+        console.error('Failed to update user:', response.data.message);
       }
-      console.log('User updated successfully!');
     } catch (error) {
       console.error('Error updating the user:', error);
     }
   }
+  
 
-  const fetchUserInfo = async (userId:any) => {
+  const getUserById = async (userId: any): Promise<IUser | null> => {
     try {
-      const response = await axios.post(`${ACCOUNT_MANAGEMENT}/api/getUserInfo`, { userId });
-      setUserInfo(response.data);
+      const response = await axios.get(`${ACCOUNT_MANAGEMENT}/api/getUserById/${userId}`);
+      return response.data;
     } catch (error) {
-      console.error('Error fetching user information:', error);
+      console.error('Error fetching user by ID:', error);
+      return null;
+    }
+  };
+
+  const fetchUser = async () => {
+    const fetchedUser = await getUserById(userId);
+    if (fetchedUser) {
+      setUser(fetchedUser);
     }
   };
 
 
-  useEffect(() =>{ 
-    if(userId){
-      fetchUserInfo(userId);
-    }
-  }, [userId]);
-
-
-
-
-  // Update user state whenever userInfo changes
   useEffect(() => {
-    setUser({
-      githubAccount: userInfo.githubAccount,
-      name: userInfo.userName,
-      lastName: userInfo.userLastName,
-    });
-  }, [userInfo]);
+    fetchUser();
+  }, [userId]);
 
 
 
@@ -106,47 +100,61 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
   };
 
 
+
+  const getUsernameFromTelegram = async (chatId: number) => {
+    try {
+      const response = await axios.get(`https://api.telegram.org/bot6572515145:AAH3lQky2jdYWs84nH0ZOf_-AnroOH3NGXs/getChat?chat_id=${chatId}`);
+      const username = response.data.result.first_name;
+      return username;
+    } catch (error) {
+      console.error('Error fetching username from Telegram:', error);
+      return null;
+    }
+  };
+
+
+
+
+
   const handleButtonClick = async () => {
     if (!isToggled) {
       (async () => {
         try {
-          // Open the Telegram bot link in a new tab
           window.open('http://t.me/sqqoiin_bot', '_blank');
-          // Retrieve the chat ID from Telegram
           const chatId = await getChatIdFromTelegram();
-          console.log(chatId);
-          // Send the chat ID to your backend
-          await axios.post('http://localhost:3006/api/telegram/chat', { chatId,userId});
-          // Perform any other desired actions
+          let telegramusername: any;
+          if (chatId) {
+            telegramusername = await getUsernameFromTelegram(chatId);
+          }
+          UpdateUserInformation(userId, Name, lastName, telegramusername);
+          await axios.post('http://localhost:3006/api/telegram/chat', { chatId, userId });
           console.log('Button clicked, chat ID collected, and sent to the backend.');
-        } catch (error:any) {
+        } catch (error: any) {
           console.error('Error handling button click:', error);
           console.log("Error handling button click" + error.message);
         }
       })();
     }
-
-    // Toggle the connected state
     setIsToggled((prevIsToggled) => !prevIsToggled);
   };
 
 
   useEffect(() => {
-   if(userId){ axios.get(`http://localhost:3006/api/telegram/chat/${userId}`)
-      .then(response => {
-        // If the server returns a response (data exists), set isToggled to true
-        setIsToggled(response.data.length > 0);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        // Handle any error that occurred during the request
-      });}
+    if (userId) {
+      axios.get(`http://localhost:3006/api/telegram/chat/${userId}`)
+        .then(response => {
+          setIsToggled(response.data.length > 0);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }
   }, [userId]);
 
-  
 
-  
-  return ( 
+
+
+  return (
     <div className="accountdetails">
       <div className="form">
         <div className="bg-white shadow rounded-lg d-block d-sm-flex">
@@ -163,10 +171,10 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
                   <div className="form-group">
                     <label htmlFor="name">Name</label>
                     <input
-                        className="form-control"
-                        id="name"
-                        defaultValue={User.name}
-                        onChange={handleNameChange}
+                      className="form-control"
+                      id="name"
+                      defaultValue={user?.name}
+                      onChange={handleNameChange}
                     ></input>
                   </div>
                 </div>
@@ -176,10 +184,10 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
                   <div className="form-group">
                     <label htmlFor="lastName">Last name </label>
                     <input
-                        className="form-control"
-                        id="lastName"
-                        defaultValue={User.lastName}
-                        onChange={handleLastNameChange}
+                      className="form-control"
+                      id="lastName"
+                      defaultValue={user?.lastName}
+                      onChange={handleLastNameChange}
                     ></input>
                   </div>
                 </div>
@@ -190,7 +198,7 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
                     <div
                       className="form-control"
                       id="email"
-                    >{userInfo.email}
+                    >{user?.email}
                     </div>
                   </div>
                 </div>
@@ -204,46 +212,41 @@ const AccountDetails: React.FC<any> = ({ userId }) => {
                     </div>
                   </div>
                 </div>
+
+
                 <div className="col-md-6">
                   <div className="form-group">
-
-                  
-
-
-                  
-
-                    {/* <label htmlFor="radioOptions">Payment Options</label>
-                    <div>
-                      <label className="radio-label">
-                        <input type="radio" id="option1" name="radioOptions" value="Payant" className="custom-radio" defaultChecked />
-                        Payant
-                      </label>
-                      <label className="radio-label">
-                        <input type="radio" id="option2" name="radioOptions" value="Non payant" className="custom-radio" disabled />
-                        Non payant
-                      </label>
-                    </div> */}
+                    <label htmlFor="lastName">Telegram User Name</label>
+                    <input
+                      className="form-control"
+                      id="lastName"
+                      defaultValue={user?.telegram_user_name}
+                    ></input>
+                  </div>
+                </div>
 
 
+                <div className="col-md-6">
+                  <div className="form-group">
                   </div>
                   <div className='button-container'>
-                  <div className='first-button-container'><button className='confirm' onClick={()=>history.push("/paymentmode")}>Payer mon compte</button>
-                    <button className='confirm' onClick={handleApplyChanges}>Save changes</button>
+                    <div className='first-button-container'><button className='confirm' onClick={() => history.push("/paymentmode")}>Payer mon compte</button>
+                      <button className='confirm' onClick={handleApplyChanges}>Save changes</button>
                     </div>
-                  <div className='on-off-container'>
-                  <span className="icons"><FaTelegram/></span>
-                    <span>Enable Telegram Notification</span>
-                    <input
-                      className='on-off-input'
-                      type="checkbox"
-                      id="switch"
-                      onClick={handleButtonClick}
-                      checked={isToggled}
-                    />
-                    <label className='on-off-label' htmlFor="switch"></label>
+                    <div className='on-off-container'>
+                      <span className="icons"><FaTelegram /></span>
+                      <span>Enable Telegram Notification</span>
+                      <input
+                        className='on-off-input'
+                        type="checkbox"
+                        id="switch"
+                        onClick={handleButtonClick}
+                        checked={isToggled}
+                      />
+                      <label className='on-off-label' htmlFor="switch"></label>
+                    </div>
                   </div>
-                  </div>
-                  
+
                 </div>
               </div>
 
