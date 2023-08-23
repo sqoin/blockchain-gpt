e35b8d0d0e5f4d86965769dc46f59f1f1e612dc0
benothmenons <benothmen.ons@esprit.tn>
2023-08-23 12:05:20 +0100

    duration

--- backend/accountManagement/models/repetitivesTasks.ts
+++ backend/accountManagement/models/repetitivesTasks.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const taskSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   task: { type: String, required: true },
-  duration:{ type: Number , required: true },
+  duration:{ type: Number , required: true,min: 0 },
   status: {type:Boolean ,required: true }
 });
 
